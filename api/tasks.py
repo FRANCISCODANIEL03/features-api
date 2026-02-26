79926385a891ec1e4f34db2735573834758b0bb0
FRANCISCODANIEL03 <L202223016@jilotepec.tecnm.mx>
2026-02-26 14:30:00 -0600

    add code

--- api/tasks.py
+++ api/tasks.py
@@ -15,3 +15,4 @@ from sklearn.cluster import KMeans, DBSCAN
 from sklearn.preprocessing import MinMaxScaler
 from sklearn import metrics
 from sklearn.metrics import f1_score, silhouette_score, calinski_harabasz_score
+from sklearn.metrics.cluster import contingency_matrix
