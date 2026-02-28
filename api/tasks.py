c7234b1b587e34a900bced92aad233ba4f2ed081
FRANCISCODANIEL03 <L202223016@jilotepec.tecnm.mx>
2026-02-28 14:30:00 -0600

    add code

--- api/tasks.py
+++ api/tasks.py
@@ -41,3 +41,4 @@ def plot_decision_boundaries(clusterer, X, y, resolution=1000, show_centroids=Tr
                          np.linspace(mins[1], maxs[1], resolution))
     
     Z = clusterer.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
