ead151ae3df7ff32a21cbc502407ebfaa17e6514
FRANCISCODANIEL03 <L202223016@jilotepec.tecnm.mx>
2026-03-11 14:30:00 -0600

    add code

--- api/tasks.py
+++ api/tasks.py
@@ -102,3 +102,5 @@ def run_rf_logic(df, user_params, top_n):
 def run_kmeans_logic(df, user_params):
     cols_drop = ['Class', 'Time', 'Amount']
     X_high = df.drop([c for c in cols_drop if c in df.columns], axis=1)
+    y = df['Class']
+    
