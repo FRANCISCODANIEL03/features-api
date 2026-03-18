f447cde2b583e9fa4305d40bc061bb3bf1f6f86b
FRANCISCODANIEL03 <L202223016@jilotepec.tecnm.mx>
2026-03-18 14:30:00 -0600

    remove code

--- api/views.py
+++ api/views.py
@@ -3,3 +3,4 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import FeatureSelectionJob
+from .tasks import run_analysis_job
