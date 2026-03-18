46127ff0bb1dafd574d46370f0801003421e07a9
FRANCISCODANIEL03 <L202223016@jilotepec.tecnm.mx>
2026-03-18 14:30:00 -0600

    remove code

--- api/views.py
+++ api/views.py
@@ -12,3 +12,8 @@ class StartJobView(APIView):
     def post(self, request, *args, **kwargs):
         job_type = request.data.get('job_type', 'RF')
         model_params = request.data.get('model_params', {})
+        
+        # --- CAPTURAR EJES ---
+        # Aseguramos que existan en model_params si el frontend no los mandó bien
+        if 'feature_x' not in model_params:
+            model_params['feature_x'] = 'V10' # Default
